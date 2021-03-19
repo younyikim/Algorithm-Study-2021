35c01e3ff19d6000a3e0762a705e8fd3a554b4bf
Hyejin Eom <yazbyz@gmail.com>
2021-03-19 16:01:00 +0900

    week04

---
+++ yazbyz/week04/BJ_15652.java
@@ -0,0 +1,40 @@
+//BaekJoon15652
+//author : Hyejin Eom
+//20210318
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_15652 {
+	public static int[] arr;
+	public static int M, N;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] temp = br.readLine().split(" ");
+		N = Integer.parseInt(temp[0]);
+		M = Integer.parseInt(temp[1]);
+		if (1 > M || M > N || N > 8) { return; }
+
+		arr = new int[M];
+
+		dfs(1, 0);
+	}
+	public static void dfs(int at, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = at; i <= N; i++) {
+			arr[depth] = i;
+			dfs(i, depth + 1);
+		}
+	}
+
+	// 출처: https://st-lab.tistory.com/115?category=862595 [Stranger's LAB]
+}
