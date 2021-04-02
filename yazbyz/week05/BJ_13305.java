99049405b2a9c7da4298e222e85f852a184d01cc
Hyejin Eom <yazbyz@gmail.com>
2021-04-02 18:10:18 +0900

    week05

---
+++ yazbyz/week05/BJ_13305.java
@@ -0,0 +1,19 @@
+//BaekJoon13305
+//author : Hyejin Eom
+//20210402
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_13305 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	}
+
+	public static int greedy(String[] bucket) {
+		int result = 0;
+		return result;
+	}
+}
