7af64cc0c812b585f9608da5258b74ab2a7da3ab
hyosunLee <leecrossun@gmail.com>
2021-04-28 21:20:50 +0900

    04/28

---
+++ leecrossun/step23/BaekJoon2629.py
@@ -0,0 +1,11 @@
+# 양팔 저울
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+weight_n = int(input()) # 추의 개수
+weight = list(map(int, input().split())) # 추의 무게
+marble_n = int(input()) # 구슬의 개수
+marble = list(map(int, input().split())) # 구슬의 무게
+
+
+
