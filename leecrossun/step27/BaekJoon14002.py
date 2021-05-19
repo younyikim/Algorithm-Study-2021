358ff1d122d6866ced7385f8b707a3504f907919
hyosunLee <leecrossun@gmail.com>
2021-05-19 13:31:37 +0900

    05/19

--- leecrossun/step27/BaekJoon14002.py
+++ leecrossun/step27/BaekJoon14002.py
@@ -1,33 +1,27 @@
 # 가장 긴 증가하는 부분 수열 4
+from collections import deque
 a = int(input())
 arr = list(map(int, input().split()))
-dp = list()
-dp.append([0,0])
-for item in arr:
-    dp.append([1, item])
-
+dp = [1] * a
 # print(dp)
 
-for i in range(1, a): # arp
+for i in range(a): # arp
     # print(i, 1, i+1)
-    for j in range(1, i+1): # dp
+    for j in range(i): # dp
         # print(arr[i],dp[j][dp[j][0]])
-        if arr[i] > dp[j][dp[j][0]]:
-            dp[j].append(arr[i])
-            dp[j][0] += 1
+        if arr[i] > arr[j] and dp[i] < dp[j]+1:
+            dp[i] = dp[j] + 1
     # print("----------------")
 
-# print(dp)
-max = 0
-max_index = 0
-for i in range(a+1):
-    # print(dp[i][0])
-    if dp[i][0] > max:
-        # print("debug")
-        max = dp[i][0]
-        max_index = i
+index = dp.index(max(dp))
+c = dp[index]
+d = deque()
+for i in range(index-1, -1, -1):
+    if c == dp[i] + 1:
+        c = dp[i]
+        d.appendleft(arr[i])
 
-print(dp[max_index][0])
-for i in range(1,dp[max_index][0]+1):
-    print(dp[max_index][i], end=' ')
+d.append(arr[index])
 
+print(max(dp))
+print(' '.join(map(str,d)))
